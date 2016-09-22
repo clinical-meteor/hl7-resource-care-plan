25c2a72c566ffc1349c99617c590637a196d6ef0
Abigail Watson <awatson1978@gmail.com>
2016-09-21 20:39:44 -0500

    remove clinical:router

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.0.2',
+  version: '1.0.4',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -22,7 +22,7 @@ Package.onUse(function (api) {
   api.addFiles('server/initialize.js', 'server');
 
     api.use('clinical:base-model@1.3.1');
-    api.use('clinical:router@2.0.17');
+    // api.use('clinical:router@2.0.17');
 
     // api.addFiles('client/components/careplanUpsertPage/careplanUpsertPage.html', ['client']);
     // api.addFiles('client/components/careplanUpsertPage/careplanUpsertPage.js', ['client']);
