a159ae9f19449a9277b1bbc88f82be537c3ed4af
Abigail Watson <awatson1978@gmail.com>
2016-09-21 22:27:10 -0500

    updated datatype rep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.0.4',
+  version: '1.0.5',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -13,7 +13,7 @@ Package.onUse(function (api) {
   api.use('mongo');
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.3.3');
-  api.use('clinical:hl7-resource-datatypes@0.2.0');
+  api.use('clinical:hl7-resource-datatypes@0.4.6');
   api.use('simple:json-routes@2.1.0');
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
