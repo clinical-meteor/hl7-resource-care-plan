3dd666d4b4013f9158bb133b5c5331fd242c5c09
Abigail Watson <awatson1978@gmail.com>
2016-10-07 16:49:59 -0500

    export CarePlanSchema

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.0.6',
+  version: '1.0.7',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -43,6 +43,7 @@ Package.onUse(function (api) {
 
   api.export('CarePlan');
   api.export('CarePlans');
+  api.export('CarePlanSchema');
 });
 
 // Package.onTest(function (api) {
