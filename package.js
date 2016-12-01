d6c6601c1e263aa0b1c4393067748161d5f37d70
Abigail Watson <awatson1978@gmail.com>
2016-12-01 12:06:34 -0600

    latest datatypes; Meteor.publish()

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.0.8',
+  version: '1.1.0',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -12,34 +12,17 @@ Package.onUse(function (api) {
   api.use('meteor-platform');
   api.use('mongo');
   api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.3.3');
-  api.use('clinical:hl7-resource-datatypes@0.4.7');
+  api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
+
+  api.use('clinical:base-model@1.3.5');
+  api.use('clinical:hl7-resource-datatypes@0.6.0');
 
   api.addFiles('lib/hl7-resource-careplan.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
-  api.use('clinical:base-model@1.3.1');
-    // api.use('clinical:router@2.0.17');
-
-    // api.addFiles('client/components/careplanUpsertPage/careplanUpsertPage.html', ['client']);
-    // api.addFiles('client/components/careplanUpsertPage/careplanUpsertPage.js', ['client']);
-    // api.addFiles('client/components/careplanUpsertPage/careplanUpsertPage.less', ['client']);
-    //
-    // api.addFiles('client/components/careplansTablePage/careplansTablePage.html', ['client']);
-    // api.addFiles('client/components/careplansTablePage/careplansTablePage.js', ['client']);
-    // api.addFiles('client/components/careplansTablePage/careplansTablePage.less', ['client']);
-    // api.addFiles('client/components/careplansTablePage/jquery.tablesorter.js', ['client']);
-    //
-    // api.addFiles('client/components/careplanPreviewPage/careplanPreviewPage.html', ['client']);
-    // api.addFiles('client/components/careplanPreviewPage/careplanPreviewPage.js', ['client']);
-    // api.addFiles('client/components/careplanPreviewPage/careplanPreviewPage.less', ['client']);
-    //
-    // api.addFiles('client/components/careplansListPage/careplansListPage.html', ['client']);
-    // api.addFiles('client/components/careplansListPage/careplansListPage.js', ['client']);
-    // api.addFiles('client/components/careplansListPage/careplansListPage.less', ['client']);
 
   api.export('CarePlan');
   api.export('CarePlans');
