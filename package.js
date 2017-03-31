5b1df67af0c847f56a16c95cb4bb03c59fb7fe7b
Abigail Watson <awatson1978@gmail.com>
2017-03-31 17:59:15 -0500

    DTSU3 datatypes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.1.0',
+  version: '1.1.2',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -14,10 +14,10 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
+  api.use('clinical:fhir-vault-server@0.0.3');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
 
   api.addFiles('lib/hl7-resource-careplan.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
