056e5d9fcb2f7f0c60922fd3fe51ac6879fe8a80
Abigail Watson <awatson1978@gmail.com>
2017-07-23 16:26:19 -0500

    routes; mainIndex; components

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.1.3',
+  version: '1.1.4',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -14,15 +14,18 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('clinical:fhir-vault-server@0.0.3');
 
   api.use('clinical:base-model@1.3.5');
   api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-bundle@1.3.10');
 
   api.addFiles('lib/hl7-resource-careplan.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
+  if(Package['clinical:fhir-vault-server']){
+    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  }
 
   api.export('CarePlan');
   api.export('CarePlans');
