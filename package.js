80dd33be21dfa16befa9ada4b6cf47073642c964
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-09-01 11:50:27 -0500

    simpl-schema updates; hipaa-logger support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.2.21',
+  version: '1.3.0',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -11,27 +11,29 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.5.0');
+  api.use('ecmascript@0.9.0');
+  
+  api.use('aldeed:collection2@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@4.0.0');
+  api.use('clinical:hl7-resource-bundle@1.4.0');
+
   api.use('simple:json-routes@2.1.0');
   api.use('momentjs:moment@2.17.1');
-  api.use('ecmascript@0.9.0');
+
   api.use('session');
   api.use('http');
   api.use('react-meteor-data@0.2.15');
 
   api.use('clinical:glass-ui@2.1.4');
   api.use('clinical:extended-api@2.2.2');
-  api.use('clinical:base-model@1.3.5');
+  api.use('clinical:base-model@1.4.0');
   api.use('clinical:user-model@1.5.0');
-  api.use('clinical:hl7-resource-datatypes@3.0.1');
-  api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('matb33:collection-hooks@0.7.15');
   api.use('clinical:alert@2.1.3')
 
-  api.use('clinical:hl7-resource-goal@1.2.16');
-  api.use('clinical:hl7-resource-medication@1.2.6');
-  api.use('clinical:hl7-resource-patient@3.2.29');
+  api.use('clinical:hl7-resource-goal@1.3.0');
+  api.use('clinical:hl7-resource-medication@1.3.0');
+  api.use('clinical:hl7-resource-patient@3.3.0');
 
   api.addFiles('lib/CarePlans.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
@@ -48,3 +50,7 @@ Package.onUse(function (api) {
 
   api.mainModule('index.jsx', 'client');
 });
+
+Npm.depends({
+  "simpl-schema": "1.5.3"
+})
