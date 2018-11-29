e178b21b8b78d3e9adf51701f01cbc6b88deacae
Abigail Watson <awatson1978@gmail.com>
2018-11-29 14:04:02 -0600

    post thanksgiving sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.3.10',
+  version: '1.3.11',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
