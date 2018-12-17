3b192a51d297dd4060977a80622c84479838fec0
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-12-16 21:01:39 -0600

    internal & external features complete; starting QA

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-careplan',
-  version: '1.3.11',
+  version: '1.3.12',
   summary: 'HL7 FHIR Resource - CarePlan',
   git: 'https://github.com/clinical-meteor/hl7-resource-careplan',
   documentation: 'README.md'
@@ -31,10 +31,10 @@ Package.onUse(function (api) {
   api.use('matb33:collection-hooks@0.7.15');
   api.use('clinical:alert@2.1.3')
 
-  api.use('clinical:hl7-resource-goal@1.3.6');
-  api.use('clinical:hl7-resource-condition@1.7.18');
-  api.use('clinical:hl7-resource-medication@1.3.5');
-  api.use('clinical:hl7-resource-patient@3.4.8');
+  api.use('clinical:hl7-resource-goal@1.3.8');
+  api.use('clinical:hl7-resource-condition@1.7.25');
+  api.use('clinical:hl7-resource-medication@1.3.7');
+  api.use('clinical:hl7-resource-patient@3.4.17');
 
   api.addFiles('lib/CarePlans.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
